81ea3218cd4309fd3bcf0b7a38952e3d6b7b0411
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-07-01 13:21:35 +0300

    sidebar dropdown fixed

--- src/src.js
+++ src/src.js
@@ -1,7 +1,7 @@
 
 //Hide and Show navbar when scrolling:
 let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.querySelector(".topnav").style.top = "0";
@@ -14,12 +14,19 @@ window.onscroll = function() {
 
 //sidenav eventlistener for dropdown menu
 
-const categories = document.querySelector('.categories');
-categories.addEventListener('click', (e) => {
-    console.log(e.target);
-        if(e.target.querySelector(".dropdown").style.display == ''){
-            e.target.querySelector(".dropdown").style.display = 'block';
-        }else if(e.target.querySelector(".dropdown").style.display == 'block'){
-            e.target.querySelector(".dropdown").style.display = '';
-        }
-});
+// const categories = document.querySelector('.categories');
+// //console.log(categories);
+// categories.addEventListener('mouseover', (e) => {
+
+//   if (e.target.querySelector(".dropdown").style.display == '') {
+//     console.log('here');
+//     e.target.querySelector(".dropdown").style.display = 'block';
+//   }
+// });
+
+// categories.addEventListener('mouseleave', (e) => {
+ 
+//     console.log('there');
+//     e.target.querySelector(".dropdown").style.display = '';
+  
+// });
