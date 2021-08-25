96c7cfd2561521970dea68cb6592c20e84f2b4b9
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-08-25 12:56:43 +0300

    topnav deletion and inserting new

--- src/src.js
+++ src/src.js
@@ -1,13 +1,13 @@
 
 //Hide and Show navbar when scrolling:
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector(".topnav").style.top = "0";
-  } else {
-    document.querySelector(".topnav").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   let currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.querySelector(".topnav").style.top = "0";
+//   } else {
+//     document.querySelector(".topnav").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
 
