856b110aa328aed51712a5cb6923d6df5782fe33
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-08-29 12:27:28 +0300

    about us page created

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
 import { render } from '../node_modules/lit-html/lit-html.js';
 import page from '../node_modules/page/page.mjs';
 
+
+
+import {aboutUsPage} from './views/aboutUs.js';
+
+
+page('/aboutUs', decorateContext, aboutUsPage);
+
+
+page.start();
+
+
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, main);
+    ctx.setUserNav = setUserNav;
+    next();
+}
