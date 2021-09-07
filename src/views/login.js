e09a54e43cd031ddf8192eb7beac47bbaf9e42ef
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-09-07 17:34:48 +0300

    background in the login page added.

--- src/views/login.js
+++ src/views/login.js
@@ -3,67 +3,69 @@ import { html } from '../../node_modules/lit-html/lit-html.js';
 
 
 const loginTemplate = () => html`
-<div class="content-login">
-    <div class="page-member-login">
-        <div class="main">
-            <div class="inner">
-                <div class="inner-inner">
-                    <a href="/" class="logo bg"></a>
-                    <h1 class="title">Вход</h1>
-                    <div class="member-form">
-                        <form method="post" class="form-horizontal" name="memberLoginForm" id="memberLoginForm">
-                            <label class="field-label">
-                                <span class="required-field">E-mail</span>
-                                <input type="text" name="email" class="required" value="">
-                            </label>
-                            <label class="field-label">
-                                <span class="required-field">Парола</span>
-                                <input type="password" name="password" class="required" value="">
-                            </label>
-                            <div class="login-btn">
-                                <button type="submit" class="button-animate-login">
-                                    <span>Вход</span>
-                                </button>
+<div class="login-wrap">
+    <div class="content-login">
+        <div class="page-member-login">
+            <div class="main">
+                <div class="inner">
+                    <div class="inner-inner">
+                        <a href="/" class="logo bg"></a>
+                        <h1 class="title">Вход</h1>
+                        <div class="member-form">
+                            <form method="post" class="form-horizontal" name="memberLoginForm" id="memberLoginForm">
+                                <label class="field-label">
+                                    <span class="required-field">E-mail</span>
+                                    <input type="text" name="email" class="required" value="">
+                                </label>
+                                <label class="field-label">
+                                    <span class="required-field">Парола</span>
+                                    <input type="password" name="password" class="required" value="">
+                                </label>
+                                <div class="login-btn">
+                                    <button type="submit" class="button-animate-login">
+                                        <span>Вход</span>
+                                    </button>
+                                </div>
+                            </form>
+                            <div class="forgotten-password">
+                                <a href="/forgotten-password" class="forgotten-pass-text">
+                                    <span>Забравена парола?</span>
+                                </a>
                             </div>
-                        </form>
-                        <div class="forgotten-password">
-                            <a href="/forgotten-password" class="forgotten-pass-text">
-                                <span>Забравена парола?</span>
-                            </a>
+
+
+                        </div>
+                        <div class="separator">
+                            <span class="separator-text">или</span>
+                        </div>
+                        <div class="media-login">
+                            <button class="facebook-login-button" type="button">
+                                <span>ВХОД С FACEBOOK</span>
+                            </button>
+                            <button class="google-login-button" type="button">
+                                <span>ВХОД С GOOGLE</span>
+                            </button>
                         </div>
 
-
+                        <div class="separator"></div>
+                        <div class="not-registered-user">
+                            <span class="not-registered-user-text">Нямате профил?</span>
+                            <a href="/register">Регистрация</a>
+                        </div>
                     </div>
-                    <div class="separator">
-                        <span class="separator-text">или</span>
+                    <div class="back-to-site">
+                        <a href="/" class="text-body-dark">
+                            <span class="back-to-site-icon"></span>
+                            <span>Назад към сайта</span>
+                        </a>
                     </div>
-                    <div class="media-login">
-                        <button class="facebook-login-button" type="button">
-                            <span>ВХОД С FACEBOOK</span>
-                        </button>
-                        <button class="google-login-button" type="button">
-                            <span>ВХОД С GOOGLE</span>
-                        </button>
-                    </div>
-
-                    <div class="separator"></div>
-                    <div class="not-registered-user">
-                        <span class="not-registered-user-text">Нямате профил?</span>
-                        <a href="/register">Регистрация</a>
-                    </div>
-                </div>
-                <div class="back-to-site">
-                    <a href="/" class="text-body-dark">
-                        <span class="back-to-site-icon"></span>
-                        <span>Назад към сайта</span>
-                    </a>
                 </div>
             </div>
         </div>
     </div>
 </div>`;
 
-export async function loginPage(ctx){
+export async function loginPage(ctx) {
     const homepage = document.querySelector('.homepage-grid');
     const benefits = document.querySelector('.benefits-wrapper');
     benefits.style.display = 'none';
