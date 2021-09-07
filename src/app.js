3e9d68f0957679ea7bc05ea22a41866ce6c9aa1f
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-09-08 00:53:10 +0300

    image hiding when using the other pages.

--- src/app.js
+++ src/app.js
@@ -15,33 +15,9 @@ page.start();
 
 
 function decorateContext(ctx, next) {
+    
     ctx.render = (content) => render(content, main);
     next();
 }
 
 
-//Login page focus and blur input elements and adding class.
-
-const emailLogin = document.querySelector('input[name="email"]');
-const passwordLogin = document.querySelector('input[name="password"]');
-
-emailLogin.addEventListener("focus", (e) => {
-    const el = e.path[1];
-    el.classList.add('focused');
-});
-
-emailLogin.addEventListener('blur', (e)=>{
-    const el = e.path[1];
-    el.classList.remove('focused');
-});
-
-
-passwordLogin.addEventListener("focus", (e) => {
-    const el = e.path[1];
-    el.classList.add('focused');
-});
-
-passwordLogin.addEventListener('blur', (e)=>{
-    const el = e.path[1];
-    el.classList.remove('focused');
-})
