115a8c8b90ff066e525df37b4cf9dc2f1db82a46
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-09-08 19:04:34 +0300

    register page created.

--- src/app.js
+++ src/app.js
@@ -21,3 +21,49 @@ function decorateContext(ctx, next) {
 }
 
 
+        //Login page focus and blur input elements and adding class.
+
+        const nameRegister = document.querySelector('input[name="name"]');
+        const emailRegister = document.querySelector('input[name="email"]');
+        const passwordRegister = document.querySelector('input[name="password"]');
+        const repeatPassRegister = document.querySelector('input[name="repeatPass"]');
+        nameRegister.addEventListener("focus", (e) => {
+            const el = e.path[1];
+            el.classList.add('focused');
+        });
+    
+        nameRegister.addEventListener('blur', (e) => {
+            const el = e.path[1];
+            el.classList.remove('focused');
+        });
+
+        emailRegister.addEventListener("focus", (e) => {
+            const el = e.path[1];
+            el.classList.add('focused');
+        });
+    
+        emailRegister.addEventListener('blur', (e) => {
+            const el = e.path[1];
+            el.classList.remove('focused');
+        });
+    
+    
+        passwordRegister.addEventListener("focus", (e) => {
+            const el = e.path[1];
+            el.classList.add('focused');
+        });
+    
+        passwordRegister.addEventListener('blur', (e) => {
+            const el = e.path[1];
+            el.classList.remove('focused');
+        });
+
+        repeatPassRegister.addEventListener("focus", (e) => {
+            const el = e.path[1];
+            el.classList.add('focused');
+        });
+    
+        repeatPassRegister.addEventListener('blur', (e) => {
+            const el = e.path[1];
+            el.classList.remove('focused');
+        });
