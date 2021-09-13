65bf3ee328af8beb4f4760f0cce38a44beda48d8
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-09-13 22:06:39 +0300

    single page notfound created

--- src/views/notfound404.js
+++ src/views/notfound404.js
@@ -1,9 +1,16 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
 
-const notfoundTemplate = () => html`<p> NOT FOUND 404 QJ MI KURA</p>`;
+const notfoundTemplate = () => html`
+<div class="page-404-wrapper">
+    <a href="/">
+        <img src="/images/image-404-car.png" alt="404 - Page not found">
+        <span class="icon"></span>
+        <span class="text">Назад към сайта</span>
+    </a>
+</div>`;
 
 
-export async function notfound(ctx){
+export async function notfound(ctx) {
     ctx.render(notfoundTemplate())
 }
