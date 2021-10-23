d23b58fb67f280d37109367d67a476ec1b4cde9d
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-23 15:13:48 +0300

    api request

--- client/src/app.js
+++ client/src/app.js
@@ -2,15 +2,13 @@ import { render } from '../node_modules/lit-html/lit-html.js';
 import page from '../node_modules/page/page.mjs';
 
 
-
 import { aboutUsPage } from './views/aboutUs.js';
 import { buyoutPage } from './views/buyout/buyout.js';
 import { buyoutInfoPage1 } from './views/buyout/info-buyoutPages.js';
 import { buyoutInfoPage2 } from './views/buyout/info-buyoutPages.js';
 import { loginPage } from './views/login.js';
-//import { notfound } from './views/notfound404.js';
+// import { notfound } from './views/notfound404.js'; //TODO MAKE HOMEPAGE AND THEN UNCOMMENT THAT.
 import { registerPage } from './views/register.js';
-
 
 
 const main = document.querySelector('main');
@@ -21,7 +19,7 @@ page('/register', decorateContext, registerPage);
 page('/izkupuvane', decorateContext, buyoutPage);
 page('/izkupuvane/info', decorateContext, buyoutInfoPage1);
 page('/izkupuvane/info2', decorateContext, buyoutInfoPage2);
-//page('*', decorateContext, notfound);
+// page('*', decorateContext, notfound);
 page.start();
 
 
--- client/src/util/util.js
+++ client/src/util/util.js
@@ -1,11 +1,11 @@
-export function addFocusClass(e){
+export function addFocusClass(e) {
     const el = e.path[1];
     el.classList.add('focused');
     return el;
 }
 
-export function removeFocusClass(e){
-    if(!e.target.value){
+export function removeFocusClass(e) {
+    if (!e.target.value) {
         const el = e.path[1];
         el.classList.remove('focused');
         return el;
