00dd40d4f1ea62addd45a00d9a738c26260b9a87
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-24 14:38:37 +0300

    login, register, logout api req

---
+++ client/src/api/data.js
@@ -0,0 +1,10 @@
+import * as api from './api.js';
+
+const host = 'http://localhost:3030';
+api.settings.host = 'http://localhost:3030';
+
+export const login = api.login;
+export const register = api.register;
+export const logout = api.logout;
+
+//App specific requests
