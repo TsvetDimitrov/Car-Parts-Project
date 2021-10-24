c77c20be0fe62d8bfb650486b3c1f77b5ec998fa
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-10-24 18:04:57 +0300

    user model

---
+++ server/src/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+let validateEmail = function (email) {
+    let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return reg.test(email);
+};
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: [true, 'Name is required!'] },
+    email: { type: String, required: [true, 'Email is required!'], validate: [validateEmail, 'Please fill a valid email address'], unique: [true, 'This email already exists!'] },
+    password: { type: String, required: [true, 'Password is required!'] }
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+
+
+
