2a514164e940b1317814bbb062919018d0a35522
Tsvetomir <cvetodimitrov@tu-sofia.bg>
2021-11-07 00:00:24 +0200

    allign add to cart button

--- client/src/views/details.js
+++ client/src/views/details.js
@@ -48,6 +48,34 @@ const detailsTemplate = (product, addCartProduct) => html `
         </div>
     </div>
 </div>
+<div class="benefits-wrapper">
+    <div class="inner">
+        <div class="benefits">
+            <ul>
+                <li>
+                    <img src="/images/express-delivery-icon.png" alt="Експресна доставка">
+                    <p>Експресна доставка</p>
+                    <p>Доставяне из цяла България.</p>
+                </li>
+                <li>
+                    <img src="/images/delivery.png" alt="Нови части">
+                    <p>Нови части всеки ден</p>
+                    <p>Ежедневно зарежваме нови части.</p>
+                </li>
+                <li>
+                    <img src="/images/satisfaction-guaranteed.png" alt="Нови части">
+                    <p>Проверено качество</p>
+                    <p>Авточасти с гаранция.</p>
+                </li>
+                <li>
+                    <img src="/images/right-of-return.png" alt="Нови части">
+                    <p>Право на връщане</p>
+                    <p>14 дни право на връщане на всяка поръчана стока.</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
 </div>
 `;
 
